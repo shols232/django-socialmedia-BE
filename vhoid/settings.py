21f5a8d8c6c23a630fbe50a00ff5056b930b8c34
shols232 <akinsolaademola232@gmail.com>
2020-11-23 01:14:18 +0100

    prepare for deploy

--- vhoid/settings.py
+++ vhoid/settings.py
@@ -108,7 +108,7 @@ if DEBUG:
         'default': {
             'BACKEND': 'channels_redis.core.RedisChannelLayer',
             'CONFIG': {
-                "hosts": [('127.0.0.1', '6379')],
+                "hosts": [config('REDIS_URL')],
             },
         },
     }
